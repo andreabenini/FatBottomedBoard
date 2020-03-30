caba7255a3a939bc05cda0acd1bba4408d50cdaa
Ben <andreabenini@users.noreply.github.com>
2020-03-30 02:31:32 +0200

    some cleanup on encoder test

--- software/docs/encoder.test/encoder.test.ino
+++ software/docs/encoder.test/encoder.test.ino
@@ -7,9 +7,9 @@
 #define dc              10
 #define reset           8
 
-#define encoderSW       2
-#define encoderA        4
-#define encoderB        3
+#define encoder_CLK     2
+#define encoder_DT      3
+#define encoder_SW      4
 
 #define XPOS            30
 #define YPOS            50
@@ -30,27 +30,27 @@ int encoderCounter = 0;
 int encoderButton = HIGH;
 
 void setup() {
-    TFTscreen.begin();
     // TFT display
+    TFTscreen.begin();
     TFTscreen.background(0, 0, 0);        // clear the screen with a black background
     TFTscreen.setTextSize(3);             // set the text size
     TFTscreen.stroke(255, 255, 255);
     TFTscreen.fillRect(RECT_X, RECT_Y, WIDTH, HEIGHT, COLOR);
 
     // Rotary encoder
-    pinMode(encoderA,   INPUT_PULLUP);
-    pinMode(encoderB,   INPUT_PULLUP);
-    pinMode(encoderSW,  INPUT_PULLUP);
-    encoderState = digitalRead(encoderA);
+    pinMode(encoder_CLK,   INPUT_PULLUP);
+    pinMode(encoder_DT,   INPUT_PULLUP);
+    pinMode(encoder_SW,  INPUT_PULLUP);
+    encoderState = digitalRead(encoder_CLK);
     sprintf(Text, "%d", encoderCounter);
     TFTscreen.text(Text, XPOS, YPOS);
 }
 
 void loop() {
     // Encoder current status
-    int status = digitalRead(encoderA);
+    int status = digitalRead(encoder_CLK);
     if (status != encoderState) {
-        if (digitalRead(encoderB) != status) {
+        if (digitalRead(encoder_DT) != status) {
             encoderCounter++;
         } else {
             encoderCounter--;
@@ -62,7 +62,7 @@ void loop() {
     }
 
     // Encoder button
-    int button = digitalRead(encoderSW);
+    int button = digitalRead(encoder_SW);
     if (button != encoderButton) {
         TFTscreen.fillRect(100, RECT_Y, 40, HEIGHT, button==HIGH? 0: COLOR_BUTTON);
         encoderButton = button;
