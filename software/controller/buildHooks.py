8303afd92364caa8d615bac979047984481e3a84
Ben <andreabenini@users.noreply.github.com>
2020-10-01 23:11:13 +0200

    documentation on this PlatformIO build hook

--- software/controller/buildHooks.py
+++ software/controller/buildHooks.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 # pylint: disable=undefined-variable
+#
+# This script is executed by PlatformIO before compiling the code, it's some sort of hook executed
+# before everything in order to manually patch few things
+#   - Make a backup copy of usb_hid/usb_private.h -> usb_hid/usb_private.h.backup (just once, the first time)
+#       It's commented but you can resume it if you're running for the first time
+#   - Change: VID,PID,Name in the usb_hid profile (file: usb_private.h)
+# NOTE: this hook is not necessary, I'm using it to change device name on firmware (I don't like the default)
+#
 import os
 import shutil
 Import("env", "projenv")
@@ -36,7 +44,7 @@ print("----------------------------------------------- Compilation setup [BEGIN]
 # usb_private.h backup
 fileName = os.path.sep.join([env['CPPPATH'][1]]+["usb_hid","usb_private.h"])
 fileNameBackup = fileName+".backup"
-shutil.copyfile(fileName, fileNameBackup)
+# shutil.copyfile(fileName, fileNameBackup)
 editUSBsettings(fileName)
 print("----------------------------------------------- Compilation setup [ END ]")
 
